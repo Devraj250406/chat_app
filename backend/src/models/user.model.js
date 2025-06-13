3c7cb289fab20002be2e9828b47c93f791588a20
Devraj Singh <smilyei123@gmail.com>
2025-06-13 12:07:35 +0530

    Added User schema for authentication

---
+++ backend/src/models/user.model.js
@@ -0,0 +1,31 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    email : {
+        type : String,
+        required : true,
+        unique : true
+    },
+
+    password : {
+        type : String,
+        required : true,
+    },
+
+    fullname : {
+        type : String,
+        required : true,
+        minlength : 6
+    },
+    profilepic : {
+        type : String,
+        default : "https://cdn-icons-png.flaticon.com/512/149/149071.png"
+    },
+}
+,{
+    timestamps : true
+}
+);
+
+const User = mongoose.model("User", userSchema);
+export default User;
